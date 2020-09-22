5b5ffc4d746d3ab3f26598b1905c4172c4c9f34d
Minsi Yang <yangminsi@gmail.com>
2020-09-22 11:43:26 +0100

    Blog rendering title and content

--- src/Blog.js
+++ src/Blog.js
@@ -1,6 +1,29 @@
 import React, { Component} from 'react';
 
 class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+      blogs: []
+    }
+  }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState( {
+      [name]: value
+    });
+  }
+
+  handleSubmit = event => {
+    const blog = { title: this.state.title, content: this.state.content }
+    this.setState({ blogs: [...this.state.blogs, blog], title: '', content: ''})
+    event.preventDefault();
+  }
 
   render() {
     return(
@@ -8,14 +31,22 @@ class Blog extends Component {
         <h1 className='heading'>A place to share your thoughts</h1>
         <form>
           <label htmlFor='title' >Add Title
-            <input type='text' id='title' name='title' />
+            <input type='text' id='title' name='title' value={this.state.title} onChange={this.handleInputChange} />
           </label>
           <br />
           <label htmlFor='content'>Your thoughts
-            <textarea id='content' name='content' row='4' col='50' />
+            <textarea id='content' name='content' row='10' col='50' value={this.state.content} onChange={this.handleInputChange} />
           </label>
-          <input type='submit' value='Publish' />
+          <input type='submit' value='Publish' onClick={this.handleSubmit} />
         </form>
+        <ul>
+          {this.state.blogs.map((blog, index) => {
+           return <li key={index}>
+              <b>{blog.title}</b>
+              <p>{blog.content}</p>
+            </li>
+          })}
+        </ul>
       </div>
     )
   }
